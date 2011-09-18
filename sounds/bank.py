6a1470249a68890c1fbf06ab9e16f18306560cba
Mathieu Sonet <contact@elasticsheep.com>
2011-09-18 10:34:09 +0200

    Add empty bank generator

--- sounds/bank.py
+++ sounds/bank.py
@@ -24,7 +24,7 @@ def generate(name, files, sampling_rate = 0):
 
         # Write the header
         output.write(struct.pack("BB", 0, 1)) # Version 0, Read-only
-        output.write(struct.pack("<H", sampling_rate)) # Padding
+        output.write(struct.pack("<H", sampling_rate)) # Sampling rate
         output.write(struct.pack("<LLL", 0, 0, 0)) # Padding
 
         # Write the slot entries
@@ -54,3 +54,38 @@ def generate(name, files, sampling_rate = 0):
     bytes = os.path.getsize(name)
     print "Bank size: %i blocks" % (bytes / 512)
 
+
+def generate_empty_rw_bank(name, nb_slots, nb_blocks_by_slot, sampling_rate = 0):
+    
+    # Write the bank content
+    with open(name, "w") as output:
+        print "Writing %s..." % name
+
+        # Write the header
+        output.write(struct.pack("BB", 0, 0)) # Version 0, Read-Write
+        output.write(struct.pack("<H", 0)) # Default sampling rate
+        output.write(struct.pack("<LLL", 0, 0, 0)) # Padding
+
+        # Write the slot entries
+        offset = 1
+        for i in range(nb_slots):
+            output.write(struct.pack("<LHH", offset, nb_blocks_by_slot, sampling_rate)) # Little endian
+            offset += nb_blocks_by_slot
+    
+        # Sector padding
+        pos = output.tell()
+        if pos > 512:
+            raise "Too many slots"
+        else:
+            padding = [0] * (512 - pos)
+            output.write(struct.pack("B" * len(padding), *padding))
+
+        # Slots padding
+        empty_block = struct.pack("B", 0) * 512
+        for i in range(nb_slots):
+            for i in range(nb_blocks_by_slot):
+                output.write(empty_block)
+
+    # Print the number of blocks in the bank
+    bytes = os.path.getsize(name)
+    print "Bank size: %i blocks" % (bytes / 512)
---
+++ sounds/record/gen.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append("..")
+import bank
+
+bank.generate_empty_rw_bank("12slots10sec.bank", 12, 156)
